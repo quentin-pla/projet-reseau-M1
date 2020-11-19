d05299ec254952ff7ffc560f10e5dac76309eba5
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-19 19:58:07 +0100

    Optimisation en sauvegardant un nom à un ID + ajout méthodes pour récupérer une adresse IPv4 ou IPv6 associée à la VM

--- src/main/java/VMUtils.java
+++ src/main/java/VMUtils.java
@@ -1,7 +1,19 @@
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Utilitaire machines virtuelles vagrant
  */
 public class VMUtils {
+    /**
+     * Liste des identifiants associés aux noms des VMs
+     */
+    private static Map<String,String> vmIDs = new HashMap<>();
+
     /**
      * Obtenir le status d'une VM spécifique
      * @param vmName nom de la VM
@@ -32,8 +44,12 @@ public class VMUtils {
      * @return identifiant vm
      */
     public static String getVMId(String vmName) {
-        String vmStatus = getVMStatus(vmName);
-        return vmStatus.substring(0,vmStatus.indexOf(' '));
+        if (!vmIDs.containsKey(vmName)) {
+            String vmStatus = getVMStatus(vmName);
+            String id = vmStatus.substring(0,vmStatus.indexOf(' '));
+            vmIDs.put(vmName,id);
+        }
+        return vmIDs.get(vmName);
     }
 
     /**
@@ -45,6 +61,46 @@ public class VMUtils {
         String addresses = execSSH(vmName, "hostname -I");
         addresses = addresses.substring(addresses.indexOf(' ') + 1, addresses.length() - 1);
         return addresses.split(" ");
+    }
+
+    /**
+     * Obtenir l'adresse IPv4 associée à la VM
+     * @param vmName nom de la VM
+     * @return adresse IPv4
+     */
+    public static String getVMIPv4Address(String vmName) {
+        String[] addresses = getVMAddresses(vmName);
+        for (String ipAddress : addresses) {
+            InetAddress address = null;
+            try {
+                address = InetAddress.getByName(ipAddress);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            if (address instanceof Inet4Address)
+                return ipAddress;
+        }
+        return null;
+    }
+
+    /**
+     * Obtenir l'adresse IPv6 associée à la VM
+     * @param vmName nom de la VM
+     * @return adresse IPv6
+     */
+    public static String getVMIPv6Address(String vmName) {
+        String[] addresses = getVMAddresses(vmName);
+        for (String ipAddress : addresses) {
+            InetAddress address = null;
+            try {
+                address = InetAddress.getByName(ipAddress);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            if (address instanceof Inet6Address)
+                return ipAddress;
+        }
+        return null;
     }
 
     /**
