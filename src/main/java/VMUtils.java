7576bbd467c0a06d709cd60c0b5359fdeff3b16a
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-19 23:01:28 +0100

    Ajout de méthodes pour obtenir l'adresse réseau de chaque adresse de la VM

--- src/main/java/VMUtils.java
+++ src/main/java/VMUtils.java
@@ -1,7 +1,5 @@
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +17,7 @@ public class VMUtils {
      * @param vmName nom de la VM
      */
     public static String getVMStatus(String vmName) {
-        String status = Command.execCommand("vagrant global-status | grep -i \".*" + vmName + " \"");
+        String status = Command.execCommand("vagrant global-status | grep -i \".*" + vmName + " \"").get(0);
         if (status.length() == 0) {
             System.err.println("ERREUR : Machine " + vmName + " introuvable.");
             System.exit(1);
@@ -57,49 +55,64 @@ public class VMUtils {
      * @param vmName nom de la VM
      * @return adresses
      */
-    public static String[] getVMAddresses(String vmName) {
-        String addresses = execSSH(vmName, "hostname -I");
+    public static ArrayList<String> getVMAddresses(String vmName) {
+        String addresses = execSSH(vmName, "hostname -I").get(0);
         addresses = addresses.substring(addresses.indexOf(' ') + 1, addresses.length() - 1);
-        return addresses.split(" ");
+        return new ArrayList<>(Arrays.asList(addresses.split(" ")));
     }
 
     /**
-     * Obtenir l'adresse IPv4 associée à la VM
+     * Obtenir les adresses IPv4 associées à la VM
      * @param vmName nom de la VM
      * @return adresse IPv4
      */
-    public static String getVMIPv4Address(String vmName) {
-        String[] addresses = getVMAddresses(vmName);
-        for (String ipAddress : addresses) {
-            InetAddress address = null;
-            try {
-                address = InetAddress.getByName(ipAddress);
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            }
-            if (address instanceof Inet4Address)
-                return ipAddress;
-        }
+    public static ArrayList<String> getVMIPv4Addresses(String vmName) {
+        ArrayList<String> addresses = getVMAddresses(vmName);
+        ArrayList<String> ipv4Addresses = new ArrayList<>();
+        for (String ipAddress : addresses)
+            if (Network.checkIPv4(ipAddress))
+                ipv4Addresses.add(ipAddress);
+        return ipv4Addresses;
+    }
+
+    /**
+     * Obtenir l'adresse réseau de l'adresse IPv4 associée à la VM
+     * @param vmName nom de la VM
+     * @return adresse réseau
+     */
+    public static String getVMIPv4NetworkAddress(String vmName) {
+        ArrayList<String> routes = execSSH(vmName, "ip r");
+        String vmIPv4Address = getVMIPv4Addresses(vmName).get(0);
+        for (String route : routes)
+            if (route.contains("proto kernel  scope link  src " + vmIPv4Address))
+                return route.substring(0,route.indexOf(' '));
         return null;
     }
 
     /**
-     * Obtenir l'adresse IPv6 associée à la VM
+     * Obtenir les adresses IPv6 associées à la VM
      * @param vmName nom de la VM
      * @return adresse IPv6
      */
-    public static String getVMIPv6Address(String vmName) {
-        String[] addresses = getVMAddresses(vmName);
-        for (String ipAddress : addresses) {
-            InetAddress address = null;
-            try {
-                address = InetAddress.getByName(ipAddress);
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            }
-            if (address instanceof Inet6Address)
-                return ipAddress;
-        }
+    public static ArrayList<String> getVMIPv6Addresses(String vmName) {
+        ArrayList<String> addresses = getVMAddresses(vmName);
+        ArrayList<String> ipv6Addresses = new ArrayList<>();
+        for (String ipAddress : addresses)
+            if (Network.checkIPv6(ipAddress))
+                ipv6Addresses.add(ipAddress);
+        return ipv6Addresses;
+    }
+
+    /**
+     * Obtenir l'adresse réseau de l'adresse IPv6 associée à la VM
+     * @param vmName nom de la VM
+     * @return adresse réseau
+     */
+    public static String getVMIPv6NetworkAddress(String vmName) {
+        ArrayList<String> routes = execSSH(vmName, "ip -6 r");
+        for (String route : routes)
+            if (route.contains("proto kernel"))
+                return route.substring(0,route.indexOf(' '));
         return null;
     }
 
@@ -107,11 +120,12 @@ public class VMUtils {
      * Exécuter une série de commandes en SSH sur la VM
      * @param vmName nom de la VM
      */
-    public static String execSSH(String vmName, String... commands) {
+    public static ArrayList<String> execSSH(String vmName, String... commands) {
         String sshCommand = "vagrant ssh " + getVMId(vmName) + " -c \"";
         for (String command : commands)
             sshCommand = sshCommand.concat(command + ";");
         sshCommand = sshCommand.concat("\"");
+        System.out.println(sshCommand);
         return Command.execCommand(sshCommand);
     }
 }
