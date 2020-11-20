fd2f8c5efc71d0bca1ee4037c2a2662e215dad56
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-20 15:13:43 +0100

    Regroupement des méthodes en grosses méthodes permettant d'initialiser les VMs

--- src/main/java/VMUtils.java
+++ src/main/java/VMUtils.java
@@ -1,131 +1,95 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Utilitaire machines virtuelles vagrant
  */
 public class VMUtils {
     /**
-     * Liste des identifiants associés aux noms des VMs
+     * Obtenir le status de chaque VM initialisée
      */
-    private static Map<String,String> vmIDs = new HashMap<>();
-
-    /**
-     * Obtenir le status d'une VM spécifique
-     * @param vmName nom de la VM
-     */
-    public static String getVMStatus(String vmName) {
-        String status = Command.execCommand("vagrant global-status | grep -i \".*" + vmName + " \"").get(0);
-        if (status.length() == 0) {
-            System.err.println("ERREUR : Machine " + vmName + " introuvable.");
+    public static void getVMsStatus() {
+        ArrayList<String> status = Command.execCommand("vagrant global-status");
+        if (status != null) {
+            for (VM vm : VM.getVMs()) {
+                for (String output : status) {
+                    if (output.contains(vm.getName() + " ")) {
+                        vm.setId(output.substring(0, output.indexOf(' ')));
+                        int beginIndexState = output.indexOf("virtualbox") + 11;
+                        String state = output.substring(beginIndexState, output.indexOf(' ', beginIndexState));
+                        vm.setTurnedOn(state.equals("running"));
+                        break;
+                    }
+                }
+                if (!vm.checkStatus()) {
+                    System.err.println("ERREUR : Machine " + vm.getName() + " introuvable.");
+                    System.exit(1);
+                }
+                if (!vm.isTurnedOn()) {
+                    System.err.println("ERREUR : Machine " + vm.getName() + " éteinte. Veuillez la démarrer.");
+                    System.exit(1);
+                }
+            }
+        } else {
+            System.err.println("ERREUR : Status des machines irrécupérable.");
             System.exit(1);
         }
-        return status;
     }
 
     /**
-     * Vérifier si une machine est en cours d'exécution
-     * @param vmName nom de la VM
+     * Obtenir les adresses IP associées aux adresses de chaque VM initialisée
      */
-    public static boolean isVMStarted(String vmName) {
-        String vmStatus = getVMStatus(vmName);
-        int beginIndexState = vmStatus.indexOf("virtualbox") + 11;
-        String state = vmStatus.substring(beginIndexState, vmStatus.indexOf(' ', beginIndexState));
-        return state.equals("running");
-    }
-
-    /**
-     * Obtenir l'identifiant d'une VM
-     * @param vmName nom de la VM
-     * @return identifiant vm
-     */
-    public static String getVMId(String vmName) {
-        if (!vmIDs.containsKey(vmName)) {
-            String vmStatus = getVMStatus(vmName);
-            String id = vmStatus.substring(0,vmStatus.indexOf(' '));
-            vmIDs.put(vmName,id);
+    public static void getVMsAddresses() {
+        Map<String, Map<String,ArrayList<String>>> sshCommands = new HashMap<>();
+        for (VM vm : VM.getVMs()) {
+            if (vm.getId() == null) {
+                System.err.println("ERREUR : ID de " + vm.getName() + " non définit.");
+                System.exit(1);
+            }
+            //Exécution et récupération des sorties commandes
+            String[] commandsToRun = new String[]{"hostname -I", "ip r","ip -6 r"};
+            sshCommands.put(vm.getName(), execSSH(vm, commandsToRun));
+            //Liste des adresses IP récupérées par hostname
+            String addresses = sshCommands.get(vm.getName()).get(commandsToRun[0]).get(0);
+            //Suppression de la première adresse étant réservée à vagrant
+            addresses = addresses.substring(addresses.indexOf(' ') + 1, addresses.length() - 1);
+            //Association des adresses avec leur adresse réseau à la VM
+            for (String ipAddress : addresses.split(" ")) {
+                if (Network.checkIPv4(ipAddress)) {
+                    for (String route : sshCommands.get(vm.getName()).get(commandsToRun[1]))
+                        if (route.contains("proto kernel  scope link  src " + ipAddress))
+                            vm.getIpv4Addresses().put(ipAddress, route.substring(0, route.indexOf(' ')));
+                }
+                else if (Network.checkIPv6(ipAddress)) {
+                    for (String route : sshCommands.get(vm.getName()).get(commandsToRun[2]))
+                        if (route.contains("proto kernel") && route.contains(ipAddress.substring(0,ipAddress.indexOf("::"))))
+                            vm.getIpv6Addresses().put(ipAddress, route.substring(0, route.indexOf(' ')));
+                }
+            }
         }
-        return vmIDs.get(vmName);
-    }
-
-    /**
-     * Récupérer les adresses IP associées à une VM
-     * @param vmName nom de la VM
-     * @return adresses
-     */
-    public static ArrayList<String> getVMAddresses(String vmName) {
-        String addresses = execSSH(vmName, "hostname -I").get(0);
-        addresses = addresses.substring(addresses.indexOf(' ') + 1, addresses.length() - 1);
-        return new ArrayList<>(Arrays.asList(addresses.split(" ")));
-    }
-
-    /**
-     * Obtenir les adresses IPv4 associées à la VM
-     * @param vmName nom de la VM
-     * @return adresse IPv4
-     */
-    public static ArrayList<String> getVMIPv4Addresses(String vmName) {
-        ArrayList<String> addresses = getVMAddresses(vmName);
-        ArrayList<String> ipv4Addresses = new ArrayList<>();
-        for (String ipAddress : addresses)
-            if (Network.checkIPv4(ipAddress))
-                ipv4Addresses.add(ipAddress);
-        return ipv4Addresses;
-    }
-
-    /**
-     * Obtenir l'adresse réseau de l'adresse IPv4 associée à la VM
-     * @param vmName nom de la VM
-     * @return adresse réseau
-     */
-    public static String getVMIPv4NetworkAddress(String vmName) {
-        ArrayList<String> routes = execSSH(vmName, "ip r");
-        String vmIPv4Address = getVMIPv4Addresses(vmName).get(0);
-        for (String route : routes)
-            if (route.contains("proto kernel  scope link  src " + vmIPv4Address))
-                return route.substring(0,route.indexOf(' '));
-        return null;
-    }
-
-    /**
-     * Obtenir les adresses IPv6 associées à la VM
-     * @param vmName nom de la VM
-     * @return adresse IPv6
-     */
-    public static ArrayList<String> getVMIPv6Addresses(String vmName) {
-        ArrayList<String> addresses = getVMAddresses(vmName);
-        ArrayList<String> ipv6Addresses = new ArrayList<>();
-        for (String ipAddress : addresses)
-            if (Network.checkIPv6(ipAddress))
-                ipv6Addresses.add(ipAddress);
-        return ipv6Addresses;
-    }
-
-    /**
-     * Obtenir l'adresse réseau de l'adresse IPv6 associée à la VM
-     * @param vmName nom de la VM
-     * @return adresse réseau
-     */
-    public static String getVMIPv6NetworkAddress(String vmName) {
-        ArrayList<String> routes = execSSH(vmName, "ip -6 r");
-        for (String route : routes)
-            if (route.contains("proto kernel"))
-                return route.substring(0,route.indexOf(' '));
-        return null;
     }
 
     /**
      * Exécuter une série de commandes en SSH sur la VM
-     * @param vmName nom de la VM
+     * @param vm machine virtuelle
      */
-    public static ArrayList<String> execSSH(String vmName, String... commands) {
-        String sshCommand = "vagrant ssh " + getVMId(vmName) + " -c \"";
+    public static Map<String,ArrayList<String>> execSSH(VM vm, String... commands) {
+        if (vm.getId() == null) {
+            System.err.println("ERREUR : ID de " + vm.getName() + " irrécupérable.");
+            System.exit(1);
+        }
+        String sshCommand = "vagrant ssh " + vm.getId() + " -c \"";
         for (String command : commands)
-            sshCommand = sshCommand.concat(command + ";");
+            sshCommand = sshCommand.concat(command + ";echo NEXT;");
         sshCommand = sshCommand.concat("\"");
-        System.out.println(sshCommand);
-        return Command.execCommand(sshCommand);
+        ArrayList<String> sshOutput = Command.execCommand(sshCommand);
+        Map<String,ArrayList<String>> commandsOutput = new LinkedHashMap<>();
+        for (String command : commands)
+            commandsOutput.put(command,new ArrayList<>());
+        int commandNumber = 0;
+        for (String output : sshOutput) {
+            if (output.equals("NEXT")) ++commandNumber;
+            else commandsOutput.get(commands[commandNumber]).add(output);
+        }
+        return commandsOutput;
     }
 }
