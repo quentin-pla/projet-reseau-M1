e39a30025f648f94402c90f62807045a6beca309
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-24 15:38:53 +0100

    Implémentation méthode récupération paquets tunnels

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ public class Main {
     /**
      * Main
      * @param args arguments
+     * 0: nom de la machine virtuelle n°1
+     * 1: nom de la machine virtuelle n°2
+     * 2: nom de l'interface du tunnel
+     * 3: adresse de l'interface du tunnel
      */
     public static void main(String[] args) {
         if (args.length == 4) {
-            new Tunnel(args[0], args[1], args[2], args[3]);
+            new Tunnel(args[0], args[1], args[2], args[3]).readPacketsFromTunnel();
         } else {
             System.err.println("ERREUR : Nombre d'arguments invalide.");
             System.out.println("Utilisation: {nom machine virtuelle n°1} {nom machine virtuelle n°2} {nom interface tunnel} {adresse ip associée au tunnel}");
@@ -13,3 +17,5 @@ public class Main {
         }
     }
 }
+
+// java Main VM1 VM3 tun0 fc00:1234:ffff::1/64
--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Utilitaire Tunnel
  */
@@ -47,29 +51,65 @@ public class Tunnel {
         VMUtils.getVMsAddresses();
         //Vérification des adresses pour la VM n°1
         if (vm1.getIpv4Addresses().isEmpty() || vm1.getIpv6Addresses().isEmpty()) {
-            System.err.println("ERREUR : " + vm1.getName() + " ne dispose pas de deux interfaces comprenant une adresse IPv4 et une adresse IPv6.");
+            System.err.println("ERREUR : " + vm1.getName() + " ne dispose pas de deux interfaces comprenant une adresse IPv4 et une adresse IPv6.\r");
             System.exit(1);
         }
         //Vérification des adresses pour la VM n°2
         if (vm2.getIpv4Addresses().isEmpty() || vm2.getIpv6Addresses().isEmpty()) {
-            System.err.println("ERREUR : " + vm2.getName() + " ne dispose pas de deux interfaces comprenant une adresse IPv4 et une adresse IPv6.");
+            System.err.println("ERREUR : " + vm2.getName() + " ne dispose pas de deux interfaces comprenant une adresse IPv4 et une adresse IPv6.\r");
             System.exit(1);
         }
-        //Création du tunnel sur la VM n°1
+        //Création de la première extremité du tunnel sur la VM n°1
         VMUtils.execParallelTask(() -> VMUtils.execSSH(vm1,
+            "sudo ip tun del " + intName,
             "sudo ip tun add " + intName + " mode sit remote " + vm2.getIpv4Addresses().get(0) + " local " + vm1.getIpv4Addresses().get(0),
             "sudo ip link set dev " + intName + " up",
             "sudo ip -6 route add " + vm2.getIpv6NetworkAddresses().get(0) + " dev " + intName + " metric 1",
             "sudo ip a add " + intAddress + " dev " + intName));
-        //Création du tunnel sur la VM n°2
+        //Création de la seconde extremité du tunnel sur la VM n°2
         VMUtils.execParallelTask(() -> VMUtils.execSSH(vm2,
+            "sudo ip tun del " + intName,
             "sudo ip tun add " + intName + " mode sit remote " + vm1.getIpv4Addresses().get(0) + " local " + vm2.getIpv4Addresses().get(0),
             "sudo ip link set dev " + intName + " up",
             "sudo ip -6 route add " + vm1.getIpv6NetworkAddresses().get(0) + " dev " + intName + " metric 1",
             "sudo ip a add " + intAddress + " dev " + intName));
-        //On attend que les tunnels soient créés
+        //On attend que les extremités du tunnel soient créées
         VMUtils.waitTasksToFinish();
         //Message de succès
-        System.out.println("Tunnel " + intName + " créé avec succès.");
+        System.out.println("Tunnel " + intName + " créé avec succès.\r");
+    }
+
+    /**
+     * Lire les paquets du tunnel depuis les deux extrémités
+     */
+    public void readPacketsFromTunnel() {
+        VMUtils.execParallelTask(() -> readPacketsFromExtremity(vm1));
+        VMUtils.execParallelTask(() -> readPacketsFromExtremity(vm2));
+    }
+
+    /**
+     * Lire les paquets du tunnel depuis une extremité
+     * @param vm extremité du tunnel
+     */
+    public void readPacketsFromExtremity(VM vm) {
+        String sshCommand = "vagrant ssh " + vm.getId() + " -c \"sudo tshark -i " + intName + "\"";
+        ProcessBuilder processBuilder = new ProcessBuilder().command("bash", "-c", sshCommand);
+        processBuilder.redirectError(ProcessBuilder.Redirect.DISCARD);
+        processBuilder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        try {
+            Process process = processBuilder.start();
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            boolean ready = false;
+            while((line = in.readLine()) != null) {
+                if (ready) System.out.println(intName + "@" + vm.getName() + "> " + line + "\r");
+                if (line.equals("Capturing on '" + intName + "'")) {
+                    ready = true;
+                    System.out.println("Capture des paquets sur " + intName + " via " + vm.getName() + "...\r");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
